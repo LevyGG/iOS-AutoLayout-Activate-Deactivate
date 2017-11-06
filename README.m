d4c6b66fb20bac3d2ffcaa5644b02065a4cd62ef
Heartbreak.jpg <137476497@qq.com>
2017-11-06 23:18:48 +0800

    添加GIF图片

--- README.m
+++ README.m
@@ -1,4 +1,4 @@
 Autolayout约束的激活与失效
 
 
- ![img](https://github.com/LevyGG/iOS-AutoLayout-Activate-Deactivate/blob/master/demo.gif)
+ ![img](https://github.com/LevyGG/iOS-AutoLayout-Activate-Deactivate/demo.gif)
